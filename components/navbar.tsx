7710f84d56a9e680cd517dfb8606537dcd04f9f5
robert-chheav <robert.chheav@gmail.com>
2024-02-06 14:32:49 +0100

    layout created

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,19 +1,21 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <>
-      <nav className="flex py-4 px-2 mx-auto rounded-xl bg-blue-200 items-center w-max">
+      <nav className="flex mx-auto rounded-xl bg-blue-200 items-center w-max">
         <ul className="flex gap-6 mx-4">
-          <li>
-            <a href="">About</a>
+          <li className="hover:bg-blue-400 rounded-xl px-3 py-2 mx-auto my-2">
+            <Link href="#">About</Link>
           </li>
-          <li>
-            <a href="">Projects</a>
+          <li className="hover:bg-blue-400 rounded-xl px-3 py-2 mx-auto my-2">
+            <Link href="#">Projects</Link>
           </li>
-          <li>
-            <a href="">Client space</a>
+          <li className="hover:bg-blue-400 rounded-xl px-3 py-2 mx-auto my-2">
+            <Link href="#">Client space</Link>
           </li>
-          <li>
-            <a href="">Chat</a>
+          <li className="hover:bg-blue-400 rounded-xl px-3 py-2 mx-auto my-2">
+            <Link href="#">Chat</Link>
           </li>
         </ul>
       </nav>
