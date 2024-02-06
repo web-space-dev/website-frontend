0a02fef59019a5b3cd8199a0f9cc550fafb3fd7e
robert-chheav <robert.chheav@gmail.com>
2024-02-06 16:11:01 +0100

    adding style

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,19 @@
 import Link from "next/link";
+import { usePathname } from 'next/navigation'
+
 
 export default function Navbar() {
+  const currentPath = usePathname();
+
   return (
     <>
       <nav className="flex mx-auto rounded-xl bg-blue-200 items-center w-max">
-        <ul className="flex gap-6 mx-4">
-          <li className="hover:bg-blue-400 rounded-xl px-3 py-2 mx-auto my-2">
-            <Link href="#">About</Link>
-          </li>
+        <ul className="flex items-center gap-6 mx-4">
+          <span className={currentPath === '#' ? 'text-white' : 'border-b-2 border-b-black w-8'}>
+            <li className="hover:bg-blue-400 rounded-xl px-3 py-2 mx-auto my-2">
+              <Link href="#" >About</Link>
+            </li>
+          </span>
           <li className="hover:bg-blue-400 rounded-xl px-3 py-2 mx-auto my-2">
             <Link href="#">Projects</Link>
           </li>
