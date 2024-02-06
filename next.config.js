6ecde146900b93deab5ddf8d47a1311bcab441b1
Eoan O Dea <odeaeoan@gmail.com>
2024-02-06 11:02:30 +0000

    Bug fixing gravitar & api issues

--- next.config.js
+++ next.config.js
@@ -2,12 +2,14 @@ if (!URL.canParse(process.env.WORDPRESS_API_URL)) {
   throw new Error(`
     Please provide a valid WordPress instance URL.
     Add to your environment variables WORDPRESS_API_URL.
-  `)
+  `);
 }
 
 const { protocol, hostname, port, pathname } = new URL(
   process.env.WORDPRESS_API_URL
-)
+);
+
+console.log("check me", hostname);
 
 /** @type {import('next').NextConfig} */
 module.exports = {
@@ -21,4 +23,4 @@ module.exports = {
       },
     ],
   },
-}
+};
