76da084ee74b55eb2098adbe6c4e3586f895f404
Eoan O Dea <odeaeoan@gmail.com>
2024-02-19 15:06:32 +0100

    showcase animation improvements

--- src/components/home/show-case.tsx
+++ src/components/home/show-case.tsx
@@ -9,9 +9,18 @@ import { IconButton } from "../global/icon-button";
 import { CustomImage } from "../global/image";
 import { useState } from "react";
 
+import { motion, useViewportScroll, useTransform } from "framer-motion";
+import { ShowcaseItem } from "./showcase/showcase-item";
+
 const StyledWrapper = styled(GridContainer)`
   margin: 40px 0;
   position: relative;
+`;
+
+const StyledMotionWrapper = styled(motion.div)`
+  position: absolute;
+  width: 100%;
+  height: 100%;
 `;
 
 const StyledTitle = styled.h2<{ color: string }>`
@@ -22,103 +31,6 @@ const StyledTitle = styled.h2<{ color: string }>`
   color: ${({ color }) => color};
 `;
 
-const StyledShowcaseWrapper = styled.div`
-  grid-column: 1 / span 12;
-  position: absolute;
-  top: 85px;
-  left: 0;
-  bottom: 0;
-  display: flex;
-  align-items: center;
-  width: 100%;
-  & div {
-    margin: 0 10px;
-  }
-`;
-
-const StyledShowcaseDetails = styled.div`
-  position: relative;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 12px;
-`;
-
-const StyledAllProjects = styled.div`
-  width: 50%;
-  height: 100%;
-  background-color: ${colors.blackLight};
-  border: 2px solid ${colors.white};
-  border-radius: 12px;
-  backdrop-filter: blur(10px);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: ${getRemSize(dimensions.headingSizes.h2)};
-`;
-
-const StyledShowcaseImage = styled.div`
-  height: 100%;
-
-  & img {
-    object-fit: cover;
-    height: 100%;
-    border-radius: 12px;
-  }
-`;
-
-const StyledShowcaseContent = styled(Link)`
-  position: absolute;
-  top: 50%;
-  bottom: 50%;
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  align-items: center;
-  margin: 0 15px;
-
-  & h3,
-  p {
-    margin: 0 15px;
-  }
-`;
-
-const StyledShowcaseTitle = styled.h3`
-  font-size: ${getRemSize(dimensions.headingSizes.h2)};
-  font-weight: 400;
-`;
-
-const StyledShowcaseCategory = styled.p`
-  font-size: ${getRemSize(dimensions.headingSizes.h4)};
-  font-weight: 400;
-`;
-
-const StyledLink = styled(Link)`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  margin: 0 15px;
-  & h3 {
-    margin: 0;
-    transition: 0.3s ease;
-  }
-
-  &:hover {
-    h3 {
-      color: ${colors.accent};
-    }
-    button {
-      background-color: ${colors.accent};
-      border-color: ${colors.accent};
-    }
-    path {
-      fill: ${colors.white};
-    }
-  }
-`;
-
 interface IShowcase {
   title: string;
   projects: Projects;
@@ -126,45 +38,34 @@ interface IShowcase {
 
 export default function Showcase({ title, projects }: IShowcase) {
   const [isOpen, setIsOpen] = useState(false);
+  const { scrollYProgress } = useViewportScroll();
+  const scale = useTransform(scrollYProgress, [0, 3], [0, 1.8]);
+
   return (
     <StyledWrapper>
       <StyledTitle color={isOpen ? colors.accent : colors.white}>
         {title}
       </StyledTitle>
-      {projects.nodes.map((project: Project, index: number) => {
-        if (index !== 1) return null;
-        return (
-          <StyledShowcaseWrapper key={index}>
-            <StyledShowcaseDetails>
-              <StyledShowcaseImage>
-                <CustomImage
-                  alt={project.featuredImage.node.altText}
-                  width={750}
-                  height={800}
-                  src={project.featuredImage.node.sourceUrl}
-                  blurDataURL={project.featuredImage.node.placeholderDataURI}
-                />
-              </StyledShowcaseImage>
-
-              <StyledShowcaseContent href={`/projects/${project.slug}`}>
-                <StyledShowcaseTitle>{project.title}</StyledShowcaseTitle>
-                <StyledShowcaseCategory>
-                  {project.projectCategories?.nodes[0]?.name}
-                </StyledShowcaseCategory>
-              </StyledShowcaseContent>
-
-              {/* <Link href={`/projects/${project.slug}`}>View project</Link> */}
-            </StyledShowcaseDetails>
-
-            <StyledAllProjects>
-              <StyledLink href="/projects">
-                <StyledShowcaseTitle>View all projects</StyledShowcaseTitle>
-                <IconButton />
-              </StyledLink>
-            </StyledAllProjects>
-          </StyledShowcaseWrapper>
-        );
-      })}
+      <StyledMotionWrapper
+        className="container"
+        style={{
+          scale,
+        }}
+      >
+        {projects.nodes.map((project: Project, index: number) => {
+          if (index !== 1) return null;
+          return (
+            <motion.div
+              key={index}
+              // style={{
+              //   scaleY: scrollYProgress,
+              // }}
+            >
+              <ShowcaseItem key={index} project={project} />
+            </motion.div>
+          );
+        })}
+      </StyledMotionWrapper>
     </StyledWrapper>
   );
 }
