24dbad296bc1eabdcc69aa7a579e5db451c5aa84
Eoan O Dea <odeaeoan@gmail.com>
2024-03-12 12:58:10 +0100

    Fixed svg warnings

--- src/icons/arrow-down.tsx
+++ src/icons/arrow-down.tsx
@@ -10,8 +10,8 @@ const ArrowDown = (props: SVGProps<SVGSVGElement>) => (
     {...props}
   >
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M28.2174 33.3333L41.5508 19.9999L39.6652 18.1143L24.4462 33.3333L28.2174 33.3333ZM22.332 33.3333L22.332 -1.31242e-05L19.6654 -1.33574e-05L19.6654 33.3333L22.332 33.3333ZM17.5508 33.3332L2.33183 18.1143L0.44621 19.9999L13.7796 33.3332L17.5508 33.3332Z"
       fill="white"
     />
