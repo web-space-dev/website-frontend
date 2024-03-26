4813bfc3b220a1902f69410764ed70d2e3d9bb49
Nevena Pancic <nevenapancic97@gmail.com>
2024-03-26 17:00:19 +0100

    added shadow on cards

--- src/components/home/Approach.tsx
+++ src/components/home/Approach.tsx
@@ -1,10 +1,11 @@
+import React, { useEffect, useRef, useState } from 'react';
 import { Approach } from "../../interfaces/home";
 import styled from "@emotion/styled";
 import { colors } from "../../styles/variables";
 import { GridContainer } from "../global/grid/GridContainer";
 import Pill from "../global/pill";
-import { useRef } from "react";
 import { motion } from "framer-motion";
+
 
 
 interface IApproach {
@@ -19,25 +20,29 @@ const StyledWrapper = styled(GridContainer)`
 const StyledHeading2 = styled.h2`
   font-weight: 521;
   grid-column: 1 / span 12;
-  `;
+`;
+
 const StyledBox = styled(motion.div)`
   grid-column: 1 / span 12;
   display: flex;
   width: 100%;
-  height: 411px;
-  border: 1px 0px 1px 0px;
+  height: 274px;
+  border: 0px 0px 5px 0px;
   // padding: 40px 0px 40px 0px;
   overflow-x:  auto;
   scrollbar-width: none;
+  position: relative;
+  overflow: auto;
 
   @media (max-width: 768px) {
     flex-direction: column;
     overflow-x: visible;
   }
 `;
+
 const StyledCard = styled(motion.div)`
   margin-top: 10px;
-  margin-right: 255px;
+  margin-right: 200px;
   width: 462px;
   min-width: 470px;
   height: 251px;
@@ -50,8 +55,35 @@ const StyledCard = styled(motion.div)`
   overflow: hidden;
   scrollbar-width: none;
 
+
   @media (max-width: 768px) {
     width: 100%;
+  }
+  ::before, ::after {
+    content: "";
+    position: absolute;
+    top: 10px;
+    bottom: 13px;
+    width: 35px;
+    background: linear-gradient(to right, rgba(255, 255, 255, 0.1), transparent);
+
+
+    @media (max-width: 768px) {
+      display: none;
+    }
+  }
+
+  ::before {
+    left: 0;
+    border-top-left-radius: 20px;
+    border-bottom-left-radius: 20px;
+  }
+
+  ::after {
+    right: 10px;
+    border-top-right-radius: 20px;
+    border-bottom-right-radius: 20px;
+    background: linear-gradient(to left, rgba(255, 255, 255, 0.1), transparent);
   }
 `;
 const StyledParagraph = styled.span`
@@ -60,10 +92,12 @@ const StyledParagraph = styled.span`
   line-height: 45px;
 `;
 export default function Approach({ items }: IApproach) {
+  const boxRef = useRef(null);
+
   return (
     <StyledWrapper>
       <StyledHeading2>Our approach</StyledHeading2>
-      <StyledBox>
+      <StyledBox ref={boxRef}>
         {items.map((item, index) => (
           <StyledCard key={index}>
             <p>
