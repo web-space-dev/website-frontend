53c36f140577a4ebb7ed9eb37527b222d812fec8
Nevena Pancic <nevenapancic97@gmail.com>
2024-04-09 16:37:20 +0200

    Skills finished on mobile size screens

--- src/components/home/skills.tsx
+++ src/components/home/skills.tsx
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
-import { SkillCategories } from "../../interfaces/home";
+import { SkillCategories, Skills as IAllSkills } from "../../interfaces/home";
 import { GridContainer } from "../global/grid/gridContainer";
 import { breakpoints, colors, dimensions } from "../../styles/variables";
 import { useState } from "react";
@@ -19,23 +19,65 @@ const StyledHeading = styled.h2`
   font-weight: 500;
   @media all and (max-width: ${breakpoints.md}px) {
     grid-column: 1 / span 12;
-    font-size: ${getRemSize(dimensions.headingSizes.medium.mobile)};
+    font-size: ${getRemSize(dimensions.headingSizes.large.mobile)};
   }
 `;
 
 const StyledSkillsWrapper = styled.div`
-  grid-column: 3 / span 8;
+  grid-column: 1 / span 12;
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
 
+const SkillsMobile = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+
+  img {
+    height: auto;
+    margin: 13px;
+  }
+
+  @media all and (max-width: ${breakpoints.md}px) {
+    img {
+      width: 180px;
+    }
+  }
+
+  @media (max-width: 700px) {
+    img {
+      width: 136px;
+    }
+  }
+
+  @media (max-width: 555px) {
+    img {
+      width: 105px;
+    }
+  }
+
+  @media (max-width: 440px) {
+    img {
+      width: 77px;
+    }
+  }
+`;
+
+const StyledText = styled.p`
+  font-size: ${getRemSize(dimensions.headingSizes.cta.mobile)};
+  text-align: center;
+`;
+
 interface ISkills {
   title: string;
   categories: SkillCategories;
+  skills: IAllSkills;
 }
 
-export default function Skills({ title, categories }: ISkills) {
+export default function Skills({ title, categories, skills }: ISkills) {
   const isDesktop = useIsDesktop();
   return (
     <StyledWrapper>
@@ -44,7 +86,22 @@ export default function Skills({ title, categories }: ISkills) {
         {isDesktop ? (
           <SkillsDesktop categories={categories} />
         ) : (
-          <p>Mobile view</p>
+          <>
+            <SkillsMobile>
+              {skills.nodes.map((skill, index) => {
+                return (
+                  // <Skill>
+                  <img
+                    key={index}
+                    src={skill.featuredImage.node.sourceUrl}
+                    alt={skill.title + "Logo"}
+                  />
+                  // </Skill>
+                );
+              })}
+            </SkillsMobile>
+            <StyledText>10+ frame works & systems</StyledText>
+          </>
         )}
       </StyledSkillsWrapper>
     </StyledWrapper>
