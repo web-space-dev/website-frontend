44b9c5ed44fceb75cd60ab02c9b14e795735e5c3
Felipe Razzini <feliperazzini95@gmail.com>
2024-04-10 15:30:27 +0200

    adjusted logic for the sliding WEBSPACE at the bottom and added media queries

--- src/components/global/footer.tsx
+++ src/components/global/footer.tsx
@@ -2,13 +2,10 @@ import { EXAMPLE_PATH } from "../../lib/constants";
 import styled from "@emotion/styled";
 import { GridContainer } from "../global/grid/gridContainer";
 import Pill from "../global/pill";
-import { breakpoints, colors, dimensions } from "../../styles/variables";
-import { IconButton } from "../global/iconButton";
+import { breakpoints, colors } from "../../styles/variables";
 import chatIcon from "../../../public/svg/icon-chat.svg";
-import webspaceFrame from "../../../public/svg/webspace-frame.svg";
-import ChatIcon from "../../icons/chatIcon";
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 import { getRemSize } from "../../styles/globalCss";
 import eoanPicture from "../../../public/eoan-picture.png";
 import {
@@ -17,17 +14,6 @@ import {
   StyledPillWrapper,
   StyledTextSpacer
 } from "../../components/home/approach";
-import { useRef } from "react";
-import {
-  motion,
-  useScroll,
-  useSpring,
-  useTransform,
-  useMotionValue,
-  useVelocity,
-  useAnimationFrame
-} from 'framer-motion';
-// import { wrap } from "@motionone/utils";
 
 const StyledWrapper = styled(GridContainer)`
   margin: 140px 0;
@@ -37,19 +23,31 @@ const StyledContent = styled.div`
   grid-column: 3 / span 8;
   padding: 0;
   margin: 0;
+
+  @media all and (max-width: ${breakpoints.md}px) {
+    grid-column: 1 / span 12;
+  }
+
+  @media all and (max-width: ${breakpoints.sm}px) {
+    grid-column: 1 / span 12;
+  }
 `;
 
 const StyledImage = styled(Image)`
   width: 44px;
   height: 44px;
-  margin: 0 1.25rem;
+  margin-left: 0.7rem;
   vertical-align: middle;
+
+  @media all and (max-width: ${breakpoints.sm}px) {
+    display: none;
+  }
 `;
 
 const StyledIconButton = styled.button`
   width: 44px;
   height: 44px;
-  margin: 0 1.25rem;
+  margin: 0.7rem;
   padding: 0;
   border: 2px solid ${colors.blackLight};
   transition: 0.3s ease;
@@ -62,87 +60,121 @@ const StyledIconButton = styled.button`
   }
 `;
 
-const StyledBottomFrameContainer = styled.div`
+const StyledLinkWrapper = styled.div`
+  display: flex;
+  height: 9px;
+  color: #F8F8F8;
+  font-weight: 400;
+  opacity: 0.5;
+  margin-bottom: 1.5rem;
+  justify-content: end;
+
+  & > a {
+    margin-right: 1.5rem;
+  }
+
+  @media all and (max-width: ${breakpoints.md}px) {
+    justify-content: center;
+  }
+
+  @media all and (max-width: ${breakpoints.sm}px) {
+    justify-content: center;
+  }
+`;
+
+const StyledSlider = styled.div`
+  height: 155px;
+  margin: auto;
+  width: 100%;
+  display: grid;
+  place-items: center;
   overflow: hidden;
-  letter-spacing: -2px;
-  margin: 0;
-  white-space: nowrap;
-  flex-wrap: nowrap;
+
+  @media all and (max-width: ${breakpoints.md}px) {
+    height: 120px;
+  }
+
+  @media all and (max-width: ${breakpoints.sm}px) {
+    height: 70px;
+  }
 `;
 
-const StyledBottomFrame = styled(motion.div)`
-  font-weight: 682;
-  font-size: 344px;
-  white-space: nowrap;
-  line-height: 155px;
-  flex-wrap: nowrap;
-  letter-spacing: 0.06em;
-  text-align: center;
-`;
+const StyledSlideTrack = styled.div`
+  display: flex;
+  width: calc(1725px * 4); // total width of all slides
+  animation: scroll 8s linear infinite;
 
-const ScrollingText = styled.span`
-  display: inline-block;
-  margin-right: 40px;
-`;
-
-interface ParallaxProps {
-  children: string;
-  baseVelocity: number;
-}
-
-function wrap(min: number, max: number, value: number) {
-  const rangeSize = max - min;
-  return ((((value - min) % rangeSize) + rangeSize) % rangeSize) + min;
-}
-
-function ParallaxText({ children, baseVelocity = 100 }: ParallaxProps) {
-  const baseX = useMotionValue(0);
-  const { scrollY } = useScroll();
-  const scrollVelocity = useVelocity(scrollY);
-  const smoothVelocity = useSpring(scrollVelocity, {
-    damping: 50,
-    stiffness: 400
-  });
-  const velocityFactor = useTransform(smoothVelocity, [-30, 60], [0, 5], {
-    clamp: false
-  });
-
-  /**
-   * This is a magic wrapping for the length of the text - you
-   * have to replace for wrapping that works for you or dynamically
-   * calculate
-   */
-  const x = useTransform(baseX, (v) => `${wrap(-100, 10, v)}%`);
-
-  const directionFactor = useRef<number>(1);
-  useAnimationFrame((t, delta) => {
-    let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
-
-    /**
-     * This is what changes the direction of the scroll once we
-     * switch scrolling directions.
-     */
-    if (velocityFactor.get() < 0) {
-      directionFactor.current = -1;
-    } else if (velocityFactor.get() > 0) {
-      directionFactor.current = 1;
+  @keyframes scroll {
+    0% {
+      transform: translateX(0);
     }
+    100% {
+      transform: translateX(-1725px);
+    }
+  }
 
-    moveBy += directionFactor.current * moveBy * velocityFactor.get();
+  @media all and (max-width: ${breakpoints.md}px) {
+    width: calc(1200px * 4);
+    animation: scroll 6s linear infinite;
 
-    baseX.set(baseX.get() + moveBy);
-  });
+    @keyframes scroll {
+      0% {
+        transform: translateX(0);
+      }
+      100% {
+        transform: translateX(-1200px);
+      }
+    }
+  }
 
-  return (
-    <StyledBottomFrameContainer>
-      <StyledBottomFrame style={{ x }}>
-        {Array(4).fill(null).map((_, i) => (
-          <ScrollingText key={i}>{children}</ScrollingText>
-        ))}
-      </StyledBottomFrame>
-    </StyledBottomFrameContainer>
-  );
-}
+  @media all and (max-width: ${breakpoints.sm}px) {
+    width: calc(800px * 4);
+    animation: scroll 4s linear infinite;
+
+    @keyframes scroll {
+      0% {
+        transform: translateX(0);
+      }
+      100% {
+        transform: translateX(-600px);
+      }
+    }
+  }
+`;
+
+const StyledSlide = styled.div`
+  width: 1725px;
+  display: flex;
+  align-items: start;
+
+  @media all and (max-width: ${breakpoints.md}px) {
+    width: 1000px;
+  }
+
+  @media all and (max-width: ${breakpoints.sm}px) {
+    width: 600px;
+  }
+`;
+
+const StyledSlideText = styled.span`
+  font-weight: 600;
+  font-size: 344px;
+  letter-spacing: 0.6rem;
+  width: 100%;
+  line-height: 72px;
+  text-shadow: 9px 0px #39979C;
+
+  @media all and (max-width: ${breakpoints.md}px) {
+    font-size: 185px;
+    line-height: 40px;
+  }
+
+  @media all and (max-width: ${breakpoints.sm}px) {
+    font-size: 103px;
+    line-height: 21.6px;
+    text-shadow: none;
+  }
+`;
 
 export default function Footer() {
   return (
@@ -155,26 +187,37 @@ export default function Footer() {
               <Pill pillText={"Interested?"} />
             </StyledPillWrapper>
             <StyledParagraphText>
-              Get in contact, have a chat with Eoan
-              <StyledImage src={eoanPicture} alt="Eoan" />
-              or chat
-              <StyledIconButton>
-                <Image src={chatIcon} alt="Chat" />
-              </StyledIconButton>
+              Get in contact, have a chat with Eoan{<StyledImage src={eoanPicture} alt="Eoan" />} or chat
+              {<Link href="/contact">
+                <StyledIconButton>
+                  <Image src={chatIcon} alt="Chat" />
+                </StyledIconButton>
+              </Link>}
               with us
             </StyledParagraphText>
           </StyledParagraphWrapper>
         </StyledContent>
       </StyledWrapper>
-      <ParallaxText baseVelocity={-12}>WEBSPACE</ParallaxText>
+      <StyledLinkWrapper>
+        <Link href="#">Legal Information</Link>
+        <Link href="#">@WebSpace 2021</Link>
+      </StyledLinkWrapper>
+      <StyledSlider>
+        <StyledSlideTrack>
+          <StyledSlide>
+            <StyledSlideText>WEBSPACE</StyledSlideText>
+          </StyledSlide>
+          <StyledSlide>
+            <StyledSlideText>WEBSPACE</StyledSlideText>
+          </StyledSlide>
+          <StyledSlide>
+            <StyledSlideText>WEBSPACE</StyledSlideText>
+          </StyledSlide>
+          <StyledSlide>
+            <StyledSlideText>WEBSPACE</StyledSlideText>
+          </StyledSlide>
+        </StyledSlideTrack>
+      </StyledSlider>
     </footer>
   );
 }
-
-// Get in contact, have a chat with Eoan
-// <Image src={eoanPicture} alt="Eoan" width={"1em"} height={"1em"} />
-// or chat
-// <StyledSpan>
-//   <Image src={chatIcon} alt="Chat" width={"1em"} height={"1em"} />
-// </StyledSpan>
-// with us
