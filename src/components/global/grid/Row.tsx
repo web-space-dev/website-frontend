51a16336320e4426f8be5dc9f3210e4406f98d72
Eoan O Dea <odeaeoan@gmail.com>
2024-04-11 14:54:47 +0200

    Added Row and Col for Grid

---
+++ src/components/global/grid/Row.tsx
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+import { breakpoints } from "../../../styles/variables";
+
+export const Row = styled.div`
+  position: relative;
+  display: grid;
+  /* grid-template-columns: repeat(auto-fit, minmax(120px, 1fr)); */
+  // 320 /2
+  /* grid-template-columns: repeat(6, minmax(160px, 1fr)); */
+  grid-template-columns: repeat(6, 1fr);
+  grid-gap: 40px;
+  margin-bottom: 40px;
+
+  @media all and (max-width: ${breakpoints.md}px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+`;
