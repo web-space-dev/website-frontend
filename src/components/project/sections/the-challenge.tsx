21dfb90c7bf7f94e3be8872f08fb57e716cd1d4e
Eoan O Dea <odeaeoan@gmail.com>
2024-04-24 20:04:00 +0200

    Set up branch & fixed bug

--- src/components/project/sections/the-challenge.tsx
+++ src/components/project/sections/the-challenge.tsx
@@ -8,20 +8,23 @@ interface IProps {
 export default function DynamicTextAndImages({ content }: IProps) {
   return (
     <>
+      <p>Neve check here</p>
       {content.map((item, index) => {
-        <p>{item.text}</p>;
-        {
-          item.image && (
-            <Image
-              key={index}
-              width={500}
-              height={200}
-              alt={`Gallery Image ${index}`}
-              loader={() => item.image.node.sourceUrl}
-              src={item.image.node.sourceUrl}
-            />
-          );
-        }
+        return (
+          <>
+            <p>{item.text}</p>
+            {item.image && (
+              <Image
+                key={index}
+                width={500}
+                height={200}
+                alt={`Gallery Image ${index}`}
+                loader={() => item.image.node.sourceUrl}
+                src={item.image.node.sourceUrl}
+              />
+            )}
+          </>
+        );
       })}
     </>
   );
