779d02fde83eb1bccd01222bafc2c606ed61eb2f
Nevena Pancic <nevenapancic97@gmail.com>
2024-04-30 14:31:27 +0200

    everything is one paragraph

--- src/components/project/sections/the-challenge.tsx
+++ src/components/project/sections/the-challenge.tsx
@@ -14,27 +14,66 @@ interface IProps {
 }
 const StyledPillWrapper = styled.div``;
 
-const StyledAnimationWrapper = styled.div`
-  position: relative;
-`;
+const StyledWrapper = styled.div``;
 
 const StyledImageWrapper = styled.div`
   & img {
+    border-radius: 26px;
+    object-fit: cover;
     position: absolute;
-    bottom: 165px;
-    right: 874px;
     width: 375px;
     height: 774px;
+
+    @media (min-width: 1530px) and (max-width: 2560px) {
+      bottom: -579px;
+      right: 873px;
+    }
+
+    @media (min-width: 1421px) and (max-width: 1530px) {
+      bottom: -585px;
+      right: 799px;
+    }
+    @media (min-width: 1301px) and (max-width: 1420px) {
+      bottom: -532px;
+      right: 799px;
+    }
+    @media (min-width: 1240px) and (max-width: 1300px) {
+      bottom: -538px;
+      right: 734px;
+    }
+
+    @media (min-width: 1145px) and (max-width: 1240px) {
+      bottom: -538px;
+      right: 710px;
+    }
+
+    @media (min-width: 1050px) and (max-width: 1145px) {
+      bottom: -496px;
+      right: 627px;
+    }
+
+    @media (min-width: 921px) and (max-width: 1050px) {
+      bottom: -489px;
+      right: 487px;
+    }
+
+    @media (max-width: 920px) {
+      bottom: -438px;
+      right: 461px;
+    }
   }
 `;
 
-const StyledParagraphImage = styled.p``;
+const StyledParagraphImage = styled.span`
+  color: ${colors.accent};
+`;
 
 const StyledParagraph = styled.p`
   font-size: ${getRemSize(dimensions.headingSizes.large.mobile)};
   line-height: 1.2;
   letter-spacing: 2px;
   font-weight: 500;
+  position: relative;
 `;
 
 export default function DynamicTextAndImages({ content }: IProps) {
@@ -58,34 +97,33 @@ export default function DynamicTextAndImages({ content }: IProps) {
             </StyledPillWrapper>
           </Col>
           <Col start={5} span={8}>
-            <StyledAnimationWrapper>
-              {content.map((item, index) => {
-                return item?.image ? (
-                  // <div
-                  //   onMouseEnter={(e) => onHover(index)}
-                  //   onMouseLeave={(e) => onHoverOver()}
-                  // >
-                  <>
-                    <StyledParagraphImage>{item.text}</StyledParagraphImage>
-                    {/* // {hoverIndex === index && ( */}
-                    <StyledImageWrapper>
-                      <Image
-                        key={index}
-                        width={375}
-                        height={774}
-                        alt={`Gallery Image ${index}`}
-                        loader={() => item.image.node.sourceUrl}
-                        src={item.image.node.sourceUrl}
-                      />
-                    </StyledImageWrapper>
-                    {/* // )} */}
-                    {/* </div> */}
-                  </>
-                ) : (
-                  <StyledParagraph>{item.text}</StyledParagraph>
-                );
-              })}
-            </StyledAnimationWrapper>
+            <StyledWrapper>
+              <StyledParagraph>
+                {content.map((item, index) => {
+                  return item?.image ? (
+                    <StyledParagraphImage
+                      onMouseEnter={(e) => onHover(index)}
+                      onMouseLeave={(e) => onHoverOver()}
+                    >
+                      {" " + item.text}
+                    </StyledParagraphImage>
+                  ) : (
+                    <>{item.text}</>
+                  );
+                })}
+              </StyledParagraph>
+              {hoverIndex && (
+                <StyledImageWrapper>
+                  <Image
+                    width={375}
+                    height={774}
+                    alt={`Gallery Image ${hoverIndex}`}
+                    loader={() => content[hoverIndex].image.node.sourceUrl}
+                    src={content[hoverIndex].image.node.sourceUrl}
+                  />
+                </StyledImageWrapper>
+              )}
+            </StyledWrapper>
           </Col>
         </>
       ) : (
@@ -96,20 +134,23 @@ export default function DynamicTextAndImages({ content }: IProps) {
             </StyledPillWrapper>
           </Col>
           <Col start={1} span={12}>
-            {content.map((item) => {
+            {content.map((item, index) => {
               return (
-                <>
-                  <StyledParagraph>{item.text}</StyledParagraph>
-                  <StyledImageWrapper>
-                    <Image
-                      width={375}
-                      height={774}
-                      alt={`Gallery Image`}
-                      loader={() => item.image.node.sourceUrl}
-                      src={item.image.node.sourceUrl}
-                    />
-                  </StyledImageWrapper>
-                </>
+                <StyledWrapper>
+                  <StyledParagraph>{item.text}</StyledParagraph>;
+                  {item.image && (
+                    <StyledImageWrapper>
+                      <Image
+                        key={index}
+                        width={375}
+                        height={774}
+                        alt={`Gallery Image ${index}`}
+                        loader={() => item.image.node.sourceUrl}
+                        src={item.image.node.sourceUrl}
+                      />
+                    </StyledImageWrapper>
+                  )}
+                </StyledWrapper>
               );
             })}
           </Col>
