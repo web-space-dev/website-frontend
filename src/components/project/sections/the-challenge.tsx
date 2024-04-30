b0a01dda77c01337558560b731115059bca40789
Nevena Pancic <nevenapancic97@gmail.com>
2024-04-30 17:58:56 +0200

    desktop version done

--- src/components/project/sections/the-challenge.tsx
+++ src/components/project/sections/the-challenge.tsx
@@ -8,154 +8,159 @@ import { useState } from "react";
 import { dimensions, breakpoints, colors } from "../../../styles/variables";
 import { getRemSize } from "../../../styles/globalCss";
 import useIsDesktop from "../../../hooks/useIsDesktop";
+import { motion } from "framer-motion";
 
 interface IProps {
   content: DynamicTextAndImage[];
 }
+
+const StyledBigWrapper = styled.div`
+  position: relative;
+`;
+
 const StyledPillWrapper = styled.div``;
 
 const StyledWrapper = styled.div``;
 
 const StyledImageWrapper = styled.div`
+  display: flex;
+  overflow-x: auto;
+  gap: 8px;
   & img {
     border-radius: 26px;
     object-fit: cover;
-    position: absolute;
-    width: 375px;
-    height: 774px;
+    width: 162px;
+    height: 343px;
+  }
+`;
+const StyledImage = styled(motion(Image))`
+  border-radius: 26px;
+  object-fit: cover;
+  width: 375px;
+  height: 774px;
+  position: absolute;
+  @media (min-width: 1350px) {
+    top: 92px;
+    left: 310px;
+  }
 
-    @media (min-width: 1530px) and (max-width: 2560px) {
-      bottom: -579px;
-      right: 873px;
-    }
+  @media (min-width: 1088px) and (max-width: 1350px) {
+    top: 92px;
+    left: 186px;
+  }
 
-    @media (min-width: 1421px) and (max-width: 1530px) {
-      bottom: -585px;
-      right: 799px;
-    }
-    @media (min-width: 1301px) and (max-width: 1420px) {
-      bottom: -532px;
-      right: 799px;
-    }
-    @media (min-width: 1240px) and (max-width: 1300px) {
-      bottom: -538px;
-      right: 734px;
-    }
-
-    @media (min-width: 1145px) and (max-width: 1240px) {
-      bottom: -538px;
-      right: 710px;
-    }
-
-    @media (min-width: 1050px) and (max-width: 1145px) {
-      bottom: -496px;
-      right: 627px;
-    }
-
-    @media (min-width: 921px) and (max-width: 1050px) {
-      bottom: -489px;
-      right: 487px;
-    }
-
-    @media (max-width: 920px) {
-      bottom: -438px;
-      right: 461px;
-    }
+  @media (max-width: 1088px) {
+    top: 92px;
+    left: 116px;
   }
 `;
 
 const StyledParagraphImage = styled.span`
   color: ${colors.accent};
-`;
+  transition: color 0.2s;
 
+  &:hover {
+    color: ${colors.white};
+  }
+  & > span {
+    text-decoration: underline;
+  }
+`;
 const StyledParagraph = styled.p`
-  font-size: ${getRemSize(dimensions.headingSizes.large.mobile)};
+  font-size: ${getRemSize(dimensions.textSizes.large.desktop)};
   line-height: 1.2;
   letter-spacing: 2px;
   font-weight: 500;
-  position: relative;
 `;
-
 export default function DynamicTextAndImages({ content }: IProps) {
   const isDesktop = useIsDesktop();
   const [hoverIndex, setHoverIndex] = useState(null);
   const onHover = (index) => {
     setHoverIndex(index);
-    console.log("hovered");
   };
 
   const onHoverOver = () => {
     setHoverIndex(null);
   };
   return (
-    <Row>
-      {isDesktop ? (
-        <>
-          <Col start={1} span={4}>
-            <StyledPillWrapper>
-              <Pill pillText="The Challenge" />
-            </StyledPillWrapper>
-          </Col>
-          <Col start={5} span={8}>
-            <StyledWrapper>
-              <StyledParagraph>
-                {content.map((item, index) => {
-                  return item?.image ? (
-                    <StyledParagraphImage
-                      onMouseEnter={(e) => onHover(index)}
-                      onMouseLeave={(e) => onHoverOver()}
-                    >
-                      {" " + item.text}
-                    </StyledParagraphImage>
-                  ) : (
-                    <>{item.text}</>
-                  );
-                })}
-              </StyledParagraph>
-              {hoverIndex && (
-                <StyledImageWrapper>
-                  <Image
+    <StyledBigWrapper>
+      <Row>
+        {isDesktop ? (
+          <>
+            <Col start={1} span={4}>
+              <StyledPillWrapper>
+                <Pill pillText="The Challenge" />
+              </StyledPillWrapper>
+            </Col>
+            <Col start={5} span={8}>
+              <StyledWrapper>
+                <StyledParagraph>
+                  {content.map((item, index) => {
+                    return item?.image ? (
+                      <StyledParagraphImage
+                        onMouseEnter={(e) => onHover(index)}
+                        onMouseLeave={(e) => onHoverOver()}
+                      >
+                        {" "}
+                        <span>{item.text}</span>
+                      </StyledParagraphImage>
+                    ) : (
+                      <>{item.text}</>
+                    );
+                  })}
+                </StyledParagraph>
+                {hoverIndex && (
+                  <StyledImage
+                    initial={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{
+                      duration: 0.5,
+                      delay: 0.2,
+                      ease: [0, 0.71, 0.2, 1.01],
+                    }}
                     width={375}
                     height={774}
                     alt={`Gallery Image ${hoverIndex}`}
                     loader={() => content[hoverIndex].image.node.sourceUrl}
                     src={content[hoverIndex].image.node.sourceUrl}
                   />
-                </StyledImageWrapper>
-              )}
-            </StyledWrapper>
-          </Col>
-        </>
-      ) : (
-        <>
-          <Col start={1} span={12}>
-            <StyledPillWrapper>
-              <Pill pillText="The Challenge" />
-            </StyledPillWrapper>
-          </Col>
-          <Col start={1} span={12}>
-            {content.map((item, index) => {
-              return (
-                <StyledWrapper>
-                  <StyledParagraph>{item.text}</StyledParagraph>;
-                  {item.image && (
-                    <StyledImageWrapper>
-                      <Image
-                        key={index}
-                        width={375}
-                        height={774}
-                        alt={`Gallery Image ${index}`}
-                        loader={() => item.image.node.sourceUrl}
-                        src={item.image.node.sourceUrl}
-                      />
-                    </StyledImageWrapper>
-                  )}
-                </StyledWrapper>
-              );
-            })}
-          </Col>
-        </>
-      )}
-    </Row>
+                )}
+              </StyledWrapper>
+            </Col>
+          </>
+        ) : (
+          <>
+            <Col start={1} span={12}>
+              <StyledPillWrapper>
+                <Pill pillText="The Challenge" />
+              </StyledPillWrapper>
+            </Col>
+            <Col start={1} span={12}>
+              <StyledWrapper>
+                {content.map((item, index) => {
+                  return (
+                    <>
+                      <StyledParagraph>{item.text}</StyledParagraph>
+                      {item.image && item.image.node && (
+                        <StyledImageWrapper>
+                          <Image
+                            key={index}
+                            width={162}
+                            height={343}
+                            alt={`Gallery Image ${index}`}
+                            loader={() => item.image.node.sourceUrl}
+                            src={item.image.node.sourceUrl}
+                          />
+                        </StyledImageWrapper>
+                      )}
+                    </>
+                  );
+                })}
+              </StyledWrapper>
+            </Col>
+          </>
+        )}
+      </Row>
+    </StyledBigWrapper>
   );
 }
